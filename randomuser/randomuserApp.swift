51f706597be62e31d8aa4363b5a881931796f81e
Pau <pballada@gmail.com>
2025-01-22 17:16:50 +0100

    Initial Commit

---
+++ randomuser/randomuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomuserApp.swift
+//  randomuser
+//
+//  Created by Pau on 22/1/25.
+//
+
+import SwiftUI
+
+@main
+struct randomuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
